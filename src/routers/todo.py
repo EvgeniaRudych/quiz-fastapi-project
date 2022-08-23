aec08cc6411e16ca259de80b9e5eb0303f55bbe1
EvgeniaRudych <evgeniar2804@gmail.com>
2022-08-24 01:17:01 +0300

    adding redis and postgres connections

--- src/routers/todo.py
+++ src/routers/todo.py
@@ -1,4 +1,10 @@
+import logging
+
 from fastapi import APIRouter
+from typing import Callable
+from fastapi import FastAPI
+from app.db.tasks import connect_to_db, close_db_connection
+from pydantic import BaseSettings
 
 router = APIRouter()
 
@@ -12,3 +18,27 @@ async def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
+
+def create_start_app_handler(app: FastAPI) -> Callable:
+    async def start_app() -> None:
+        await connect_to_db(app)
+
+    return start_app
+
+
+def create_stop_app_handler(app: FastAPI) -> Callable:
+    async def stop_app() -> None:
+        await close_db_connection(app)
+
+    return stop_app
+
+
+class Config(BaseSettings):
+    # The default URL expects the app to run using Docker and docker-compose.
+    redis_url: str = 'redis://redis:6379'
+
+
+log = logging.getLogger(__name__)
+config = Config()
+app = FastAPI(title='FastAPI Redis Tutorial')
+redis = aioredis.from_url(config.redis_url, decode_responses=True)
